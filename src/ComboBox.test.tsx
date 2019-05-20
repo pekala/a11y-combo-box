75884b11a34b98f1f9e7d568e4fa739533cf9b08
Maciek Pekala <m@peka.la>
2019-05-20 21:12:26 +0200

    Add first test to assert on initial render

--- src/ComboBox.test.tsx
+++ src/ComboBox.test.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
+import { render, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
 import ComboBox from "./ComboBox";
 
+afterEach(cleanup);
+
 describe("Combo Box", () => {
-  it(`works`, () => {
-    expect(<ComboBox label="Select Fruit" />).toBeInstanceOf(Object);
+  it(`initially renders a comboxbox with closed dropdown`, () => {
+    const utils = render(<ComboBox label="Select Fruit" />);
+    const comboBox = utils.getByRole("combobox");
+    const listBox = utils.getByRole("listbox");
+    expect(comboBox).toHaveAttribute("aria-expanded", "false");
+    expect(listBox).toBeEmpty();
   });
 });
