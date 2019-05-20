e414cd86ddd5835fd3e36bdc17917b52df93a0f8
Maciek Pekala <m@peka.la>
2019-05-20 20:47:53 +0200

    Add markup from WAI-ARIA example

--- src/ComboBox.tsx
+++ src/ComboBox.tsx
@@ -1,7 +1,50 @@
 import * as React from "react";
 
-const ComboBox = () => {
-  return <div>Hello!</div>;
+type ComboBoxProps = {
+  id: string;
+  label: string;
+  triggerLabel?: string;
+};
+
+const ComboBox: React.FC<ComboBoxProps> = ({
+  id,
+  label,
+  triggerLabel = "Show options"
+}) => {
+  return (
+    <>
+      <label id={`${id}-label`}>{label}</label>
+      <div>
+        <div
+          role="combobox"
+          aria-expanded={false}
+          aria-owns={`${id}-listbox`}
+          aria-haspopup="listbox"
+          id={`${id}-combobox`}
+        >
+          <input
+            type="text"
+            aria-autocomplete="both"
+            aria-controls={`${id}-listbox`}
+            aria-labelledby={`${id}-label`}
+            id={`${id}-input`}
+          />
+          <button
+            id={`${id}-combobox-arrow`}
+            tabIndex={-1}
+            aria-label={triggerLabel}
+          >
+            ⬇
+          </button>
+        </div>
+        <ul
+          aria-labelledby={`${id}-label`}
+          role="listbox"
+          id={`${id}-listbox`}
+        />
+      </div>
+    </>
+  );
 };
 
 export default ComboBox;
