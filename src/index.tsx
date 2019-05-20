385595f30d9e17556dd0c0e06ce2e401a676459e
Maciek Pekala <m@peka.la>
2019-05-20 20:28:52 +0200

    Render the ComboBox in the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { render } from "react-dom";
+import ComboBox from "./ComboBox";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <ComboBox />
     </div>
   );
 }
